dfcec4a145a39c6fbe3fe2c86638dd17b0d9a149
AshikLikhon <mdarlikhon317@gmail.com>
2025-08-26 12:53:49 +0600

    update views.py

--- account/views.py
+++ account/views.py
@@ -4,8 +4,7 @@ from rest_framework import status
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .serializers import MyUserSerializer, UserLoginSerializer, UserProfileSerializer, ChangePasswordSerializer, \
-    SendPasswordResetEmailSerializer
+from .serializers import MyUserSerializer, UserLoginSerializer, UserProfileSerializer, ChangePasswordSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.exceptions import AuthenticationFailed
 
